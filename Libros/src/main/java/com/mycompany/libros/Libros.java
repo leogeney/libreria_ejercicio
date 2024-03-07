f3a38e8a16d10cae0a3f300c1299c8cc2a9bf34a
leito <ljgeneyl@ufpso.edu.co>
2024-03-07 09:03:44 -0500

    arreglo de libreria

--- Libros/src/main/java/com/mycompany/libros/Libros.java
+++ Libros/src/main/java/com/mycompany/libros/Libros.java
@@ -1,6 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.libros;
 
@@ -14,227 +12,89 @@ public class Libros {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        
         int opc;
         Character O;
-        
-        
-                
-                
+      
         libro libro1 = new libro();
         libro libro2 = new libro();
         libro libro3 = new libro();
         libro libro4 = new libro();
         libro libro5 = new libro();
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
+     
         //libro #1
-        libro1.title = "100 años de soledad";
-        libro1.author = "Gabriel Garcia Marquez";
-        libro1.genre = "Novela, Ficcion...etc"; 
-        libro1.avaliable = "DISPONIBLE";
-        libro1.quantity= 9;
-        libro1.loantime = "12 DIAS";
-        libro1.user = 192210;
-        libro1.returnDate = "24 de febrero";
-        libro1.loandate = "12 de febrero";
-        
+                libro1.datosLibro("100 años de soledad", "Gabriel Garcia Marquez","Novela, Ficcion...etc", "1967", "DISPONIBLE", 9, "12 DIAS", 192210, "12 de febrero", "24 de febrero");
+
         //libro #2
-        libro2.title = "El extranjero";
-        libro2.author = "Albert Camus";
-        libro2.genre = "Novela, Ficcion filosofica...etc"; 
-        libro2.avaliable = "DISPONIBLE";
-        libro2.quantity= 2;
-        libro2.loantime = " 3 DIAS";
-        libro2.user = 192209;
-        libro2.returnDate = "20 de febrero";
-        libro2.loandate = "17 de febrero";
+                libro2.datosLibro("El extranjero", "Albert Camus", "Novela, Ficcion filosofica...etc", "1942", "DISPONIBLE", 2,"3 DIAS", 192211, "17 de febrero", "20 de febrero");
         
         //libro #3
-        libro3.title = "Guerra y paz";
-        libro3.author = "León Tolstói";
-        libro3.genre = "Novela historica, Ficcion filosofica...etc"; 
-        libro3.avaliable = "NO DIPONIBLE";
-        libro3.quantity= 0;
-        libro3.loantime = "OCHO DIAS";
-        libro3.user = 192211;
-        libro3.returnDate = "10 de febrero";
-        libro3.loandate = "2 de febrero";
+                libro3.datosLibro("Guerra y paz", "Leo Tolstói","Novela historica, Ficcion filosofica...etc", "1867", "NO DISPONIBLE", 0, "OCHO DIAS", 192121, "2 de febrero", "10 de febrero");
         
         //libro #4
-        libro4.title = "Orgullo y prejucio";
-        libro4.author = "Jane Austen";
-        libro4.genre = "Novela Rosa,Satira"; 
-        libro4.avaliable = "DISPONIBLE";
-        libro4.quantity= 1;
-        libro4.loantime = "15 DIAS";
-        libro4.user = 192244;
-        libro4.returnDate = "15 de febrero";
-        libro4.loandate = "2 de marzo";
-        
-        
+                libro4.datosLibro("Orgullo y prejucio", "Jane Austen", "Novela Rosa,Satira", "1813", "DISPONIBLE", 1, "15 DIAS",182121, "15 de febrero", "2 de marzo");
         
         //libro #5
-        libro5.title = "EL DIARIO DE ANA FRANK";
-        libro5.author = "Ana Frank";
-        libro5.genre = "Biografia, Narrativa personal...etc"; 
-        libro5.avaliable = "DISPONIBLE";
-        libro5.quantity= 6;
-        libro5.loantime = "4 DIAS";
-        libro5.user = 192212;
-        libro5.returnDate = "8 de febrero";
-        libro5.loandate = "12 de febrero";
-        
-          
+                libro5.datosLibro("EL DIARIO DE ANA FRANK", "Ana Frank", "Biografia, Narrativa personal...etc", "1947", "DISPONIBLE", 6, "4 DIAS", 192112, "8 de febrero", "12 de febrero");
         
         
-        
+    //elegir opcion del libro
         System.out.println("LIBRERIA LEYENDO");
         System.out.println("1. 100 AÑOS DE SOLEDAD");
         System.out.println("2. EL EXTRANJERO");
         System.out.println("3.GUERRA Y PAZ");
         System.out.println("4. ORGULLO Y PREJUCIO");
         System.out.println("5. EL DIARIO DE ANA FRANK");
-       opc = entrada.nextInt();
+        
+         opc = entrada.nextInt();
          
         switch(opc){
             case 1 : 
-                
-                System.out.println(libro1.title );
-                System.out.println(libro1.author);
-                System.out.println(libro1.genre);
-                System.out.println(libro1.avaliable);
-                System.out.println(libro1.quantity);
-                System.out.println(libro1.loantime);
-                System.out.println(libro1.user);
-                System.out.println(libro1.returnDate);
-                System.out.println(libro1.loandate);
-                
-                
-                System.out.println("ENTREGADO MARQUE 'E'  O RECIBIDO MARQUE 'R'" );
-                O = entrada.next().charAt(0);
-                if ( O == 'E'){
-                
-                libro1.lendBook();
-                }
-                if( O == 'R'){
-                libro1.returnBook();
-                }
-                
-    
-                break;
+                    libro1.datosLibro("100 años de soledad", "Gabriel Garcia Marquez","Novela, Ficcion...etc", "", "DISPONIBLE", 9, "12 DIAS", 192210, "12 de febrero", "24 de febrero");
+
+                   
+                    
+                    break;
     
             case 2 : 
-                System.out.println(libro2.title );
-                System.out.println(libro2.author);
-                System.out.println(libro2.genre);
-                System.out.println(libro2.avaliable);
-                System.out.println(libro2.quantity);
-                System.out.println(libro2.loantime);
-                System.out.println(libro2.user);
-                System.out.println(libro2.returnDate);
-                System.out.println(libro2.loandate);
+                    libro2.datosLibro("El extranjero", "Albert Camus", "Novela, Ficcion filosofica...etc", "", "DISPONIBLE", 2,"3 DIAS", 192211, "17 de febrero", "20 de febrero");
+                    
+                    
                 
-                
-                System.out.println("ENTREGADO MARQUE 'E'  O RECIBIDO MARQUE 'R'" );
-                O = entrada.next().charAt(0);
-                if ( O == 'E'){
-                
-                libro1.lendBook();
-                }
-                if( O == 'R'){
-                libro1.returnBook();
-                }
-                
-                break;
+                    break;
         
-        case 3 : 
-        System.out.println(libro1.title );
-                System.out.println(libro3.author);
-                System.out.println(libro3.genre);
-                System.out.println(libro3.avaliable);
-                System.out.println(libro3.quantity);
-                System.out.println(libro3.loantime);
-                System.out.println(libro3.user);
-                System.out.println(libro3.returnDate);
-                System.out.println(libro3.loandate);
+            case 3 : 
+                   libro3.datosLibro("Guerra y paz", "Leo Tolstói","Novela historica, Ficcion filosofica...etc", "", "NO DISPONIBLE", 0, "OCHO DIAS", 192121, "2 de febrero", "10 de febrero");
+
+                   
+                
+                   break;
                 
                 
-                System.out.println("ENTREGADO MARQUE 'E'  O RECIBIDO MARQUE 'R'" );
-                O = entrada.next().charAt(0);
-                if ( O == 'E'){
+            case 4 : 
+                   libro4.datosLibro("Orgullo y prejucio", "Jane Austen", "Novela Rosa,Satira", "", "DISPONIBLE", 1, "15 DIAS",182121, "15 de febrero", "2 de marzo");
+
+                   
                 
-                libro1.lendBook();
-                }
-                if( O == 'R'){
-                libro1.returnBook();
-                }
-                
-                break;
-                
-                
-        case 4 : 
-             
-                System.out.println(libro4.title );
-                System.out.println(libro4.author);
-                System.out.println(libro4.genre);
-                System.out.println(libro4.avaliable);
-                System.out.println(libro4.quantity);
-                System.out.println(libro4.loantime);
-                System.out.println(libro4.user);
-                System.out.println(libro4.returnDate);
-                System.out.println(libro4.loandate);
-                
-                
-                System.out.println("ENTREGADO MARQUE 'E'  O RECIBIDO MARQUE 'R'" );
-                O = entrada.next().charAt(0);
-                if ( O == 'E'){
-                
-                libro1.lendBook();
-                }
-                if( O == 'R'){
-                libro1.returnBook();
-                }
-                
-                
-               break;
+                   break;
                
                
         case 5 :
-                System.out.println(libro5.title );
-                System.out.println(libro5.author);
-                System.out.println(libro5.genre);
-                System.out.println(libro5.avaliable);
-                System.out.println(libro5.quantity);
-                System.out.println(libro5.loantime);
-                System.out.println(libro5.user);
-                System.out.println(libro5.returnDate);
-                System.out.println(libro5.loandate);
+                libro5.datosLibro("EL DIARIO DE ANA FRANK", "Ana Frank", "Biografia, Narrativa personal...etc", "", "DISPONIBLE", 6, "4 DIAS", 192112, "8 de febrero", "12 de febrero");
+
                 
-                System.out.println("ENTREGADO MARQUE 'E'  O RECIBIDO MARQUE 'R'" );
-                O = entrada.next().charAt(0);
-                if ( O == 'E'){
                 
-                libro1.lendBook();
-                }
-                if( O == 'R'){
-                libro1.returnBook();
-                }
+                break;
     
-    } 
-       
+        }
         
-        
-        
-        
-        
+         System.out.println("ENTREGADO MARQUE 'E'  O RECIBIDO MARQUE 'R'" );
+                    O = entrada.next().charAt(0);
+                    if ( O == 'E' || O == 'e'){
+                    libro1.lendBook();
+                    }
+                    if( O == 'R' || O == 'r'){
+                    libro1.returnBook();
+                    }
     }
 }
--- Libros/src/main/java/com/mycompany/libros/libro.java
+++ Libros/src/main/java/com/mycompany/libros/libro.java
@@ -15,11 +15,36 @@ public class libro {
     String datePub;
     String avaliable;
     int quantity;
-    String loantime;
+    String loanTime;
     int user;
     String loandate;
     String returnDate;
     
+    
+    void datosLibro(String  title , String author , String genre, String datePub , String avaliable , int quantity, String loanTime, int user , String loandate , String returnDate){
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.datePub = datePub;
+    this.avaliable = avaliable;
+    this.quantity = quantity;
+    this.loanTime =loanTime;
+    this.user = user;
+    this.loandate = loandate;
+    this.returnDate = returnDate;
+    
+        System.out.println("Title: " +title);
+        System.out.println("Author: " + author);
+        System.out.println("Genre: " + genre);
+        System.out.println("Date-Pub: " + datePub);
+        System.out.println("Avaliable: " + avaliable);
+        System.out.println("Quantity: " + quantity);
+        System.out.println("LoanTime: " +loanTime);
+        System.out.println("User: " + user);
+        System.out.println("Loan-date :" + loandate);
+        System.out.println("Return-date: "+ returnDate);
+    
+    }
     
     
     void lendBook(){
