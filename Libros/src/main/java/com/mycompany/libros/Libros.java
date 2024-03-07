6d3237031d5ea69f37f14794b2ad211f22ff7de0
leogeney <ljgeneyl@ufpso.edu.co>
2024-03-07 15:49:07 -0500

    metodo de las horas agregadas

--- Libros/src/main/java/com/mycompany/libros/Libros.java
+++ Libros/src/main/java/com/mycompany/libros/Libros.java
@@ -14,7 +14,7 @@ public class Libros {
         Scanner entrada = new Scanner(System.in);
         int opc;
         Character O;
-      
+       
         libro libro1 = new libro();
         libro libro2 = new libro();
         libro libro3 = new libro();
@@ -22,24 +22,11 @@ public class Libros {
         libro libro5 = new libro();
         
      
-        //libro #1
-                libro1.datosLibro("100 años de soledad", "Gabriel Garcia Marquez","Novela, Ficcion...etc", "1967", "DISPONIBLE", 9, "12 DIAS", 192210, "12 de febrero", "24 de febrero");
-
-        //libro #2
-                libro2.datosLibro("El extranjero", "Albert Camus", "Novela, Ficcion filosofica...etc", "1942", "DISPONIBLE", 2,"3 DIAS", 192211, "17 de febrero", "20 de febrero");
-        
-        //libro #3
-                libro3.datosLibro("Guerra y paz", "Leo Tolstói","Novela historica, Ficcion filosofica...etc", "1867", "NO DISPONIBLE", 0, "OCHO DIAS", 192121, "2 de febrero", "10 de febrero");
-        
-        //libro #4
-                libro4.datosLibro("Orgullo y prejucio", "Jane Austen", "Novela Rosa,Satira", "1813", "DISPONIBLE", 1, "15 DIAS",182121, "15 de febrero", "2 de marzo");
-        
-        //libro #5
-                libro5.datosLibro("EL DIARIO DE ANA FRANK", "Ana Frank", "Biografia, Narrativa personal...etc", "1947", "DISPONIBLE", 6, "4 DIAS", 192112, "8 de febrero", "12 de febrero");
+       
         
         
     //elegir opcion del libro
-        System.out.println("LIBRERIA LEYENDO");
+        System.out.println("Libros en disponibilidad");
         System.out.println("1. 100 AÑOS DE SOLEDAD");
         System.out.println("2. EL EXTRANJERO");
         System.out.println("3.GUERRA Y PAZ");
@@ -50,48 +37,44 @@ public class Libros {
          
         switch(opc){
             case 1 : 
-                    libro1.datosLibro("100 años de soledad", "Gabriel Garcia Marquez","Novela, Ficcion...etc", "", "DISPONIBLE", 9, "12 DIAS", 192210, "12 de febrero", "24 de febrero");
+                    libro1.datosLibro("100 años de soledad", "Gabriel Garcia Marquez","Novela, Ficcion...etc", "", "DISPONIBLE", 9, 12, "192210");
 
                    
-                    
                     break;
     
             case 2 : 
-                    libro2.datosLibro("El extranjero", "Albert Camus", "Novela, Ficcion filosofica...etc", "", "DISPONIBLE", 2,"3 DIAS", 192211, "17 de febrero", "20 de febrero");
+                    libro2.datosLibro("El extranjero", "Albert Camus", "Novela, Ficcion filosofica...etc", "", "DISPONIBLE", 2,3, "192211" );
                     
                     
-                
                     break;
         
             case 3 : 
-                   libro3.datosLibro("Guerra y paz", "Leo Tolstói","Novela historica, Ficcion filosofica...etc", "", "NO DISPONIBLE", 0, "OCHO DIAS", 192121, "2 de febrero", "10 de febrero");
+                   libro3.datosLibro("Guerra y paz", "Leo Tolstói","Novela historica, Ficcion filosofica...etc", "", "NO DISPONIBLE", 0, 4, "192121");
 
-                   
                 
                    break;
                 
                 
             case 4 : 
-                   libro4.datosLibro("Orgullo y prejucio", "Jane Austen", "Novela Rosa,Satira", "", "DISPONIBLE", 1, "15 DIAS",182121, "15 de febrero", "2 de marzo");
+                   libro4.datosLibro("Orgullo y prejucio", "Jane Austen", "Novela Rosa,Satira", "", "DISPONIBLE", 1, 5,"182121");
 
-                   
                 
                    break;
                
                
         case 5 :
-                libro5.datosLibro("EL DIARIO DE ANA FRANK", "Ana Frank", "Biografia, Narrativa personal...etc", "", "DISPONIBLE", 6, "4 DIAS", 192112, "8 de febrero", "12 de febrero");
+                libro5.datosLibro("EL DIARIO DE ANA FRANK", "Ana Frank", "Biografia, Narrativa personal...etc", "", "DISPONIBLE", 6, 8, "192112" );
 
-                
-                
-                break;
+
+                    break;
     
+
         }
-        
          System.out.println("ENTREGADO MARQUE 'E'  O RECIBIDO MARQUE 'R'" );
                     O = entrada.next().charAt(0);
                     if ( O == 'E' || O == 'e'){
                     libro1.lendBook();
+                    
                     }
                     if( O == 'R' || O == 'r'){
                     libro1.returnBook();
--- Libros/src/main/java/com/mycompany/libros/libro.java
+++ Libros/src/main/java/com/mycompany/libros/libro.java
@@ -3,25 +3,37 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.libros;
+import java.awt.print.Book;
+import java.text.ParseException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author SCIS2PC09
  */
 public class libro {
+    Scanner entrada = new Scanner(System.in);
     String title;
     String author;
     String genre;
     String datePub;
     String avaliable;
     int quantity;
-    String loanTime;
-    int user;
+    int loanTime;
+    String user;
     String loandate;
     String returnDate;
+
     
     
-    void datosLibro(String  title , String author , String genre, String datePub , String avaliable , int quantity, String loanTime, int user , String loandate , String returnDate){
+    
+    void datosLibro(String  title , String author , String genre, String datePub , String avaliable , int quantity, int loanTime, String user ){
     this.title = title;
     this.author = author;
     this.genre = genre;
@@ -33,6 +45,56 @@ public class libro {
     this.loandate = loandate;
     this.returnDate = returnDate;
     
+      
+    }
+    
+    
+    void lendBook(){
+        System.out.println("LIBRO ENTREGADO");
+        this.quantity --;
+        SimpleDateFormat fechaf = new SimpleDateFormat("dd/mm/yyyy");
+               
+               Date fecha = new Date();
+              Calendar c1 = Calendar.getInstance();
+             
+              String fecha_H = String.valueOf(c1.get(Calendar.DATE))+"/"+String.valueOf(c1.get(Calendar.MONTH)+1)+"/"+String.valueOf(c1.get(Calendar.YEAR));
+            try {
+
+                Date fechaHoy = fechaf.parse(fecha_H);
+               
+                long sumaFecha = fechaHoy.getTime() + (this.loanTime*24*60*60*1000); //convierto loanTime en milisegundos
+                 Instant instant = Instant.ofEpochMilli(sumaFecha);
+                 Date myDate = Date.from(instant);
+                 this.loandate = fecha_H;
+                 this.returnDate = fechaf.format(myDate);
+
+               
+            } catch (ParseException ex) {
+                Logger.getLogger(Book.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            System.out.println("¿a quien se le entrega?");
+            user = entrada.nextLine();
+            System.out.println("Title: " +title);
+            System.out.println("Author: " + author);
+            System.out.println("Genre: " + genre);
+            System.out.println("Date-Pub: " + datePub);
+            System.out.println("Avaliable: " + avaliable);
+            System.out.println("Quantity: " + quantity);
+            System.out.println("LoanTime: " +loanTime);
+            System.out.println("User: " + user);
+            System.out.println("Loan-date :" + loandate);
+            System.out.println("Return-date: "+ returnDate);
+
+    }
+    
+    
+    void returnBook(){
+    
+        System.out.println("LIBRO RECIBIDO");
+        this.quantity++;
+        System.out.println("¿a quien se le entrega?");
+            user = entrada.nextLine();
         System.out.println("Title: " +title);
         System.out.println("Author: " + author);
         System.out.println("Genre: " + genre);
@@ -43,18 +105,8 @@ public class libro {
         System.out.println("User: " + user);
         System.out.println("Loan-date :" + loandate);
         System.out.println("Return-date: "+ returnDate);
-    
     }
     
     
-    void lendBook(){
-        System.out.println("LIBRO ENTREGADO");
     
-    }
-    
-    
-    void returnBook(){
-    
-        System.out.println("LIBRO RECIBIDO");
-    }
 }
